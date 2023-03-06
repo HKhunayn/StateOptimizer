14e8ea8a7ee2836d3e3dacf7d9d76825ed8ad544
Hisham khunayn <102166198+HKunayn@users.noreply.github.com>
2023-03-06 06:50:25 +0300

    Add files via upload

--- PeformenceManger.cs
+++ PeformenceManger.cs
@@ -12,7 +12,7 @@ public class PeformenceManger : MonoBehaviour
     [SerializeField] float checkTime = 1f;
     [Min(0), Tooltip("How many moving pixel needed for the mouse to active")]
     [SerializeField] float allowedPixelChange = 2f;
-    [Min(0), Tooltip("will not be working in Editor")]
+    [Min(0), Tooltip("will not be working in Editor if enabled")]
     [SerializeField] bool disableInEditor= true;
 
     [Header("Active Mode:")]
